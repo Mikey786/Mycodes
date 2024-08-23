42a0022229229e98442866270da188e1956a1139
Mohan krishna Narra <134443025+Mikey786@users.noreply.github.com>
2024-08-23 12:04:40 +0530

    Add files via upload

---
+++ Decode.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Decode {
+public  static void main(String[] args) {
+		String str = "45ad";
+		System.out.println("The replaced alphabet is" +str.replaceAll("[abc]", "2").replaceAll("[def]", "3"));	
+		
+		
+		
+		
+		
+	}
+
+}
